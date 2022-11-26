d10ae42e27223ac10f723b60baf838be1034c222
6Jeff9 <ceronarandia@gmail.com>
2022-11-25 21:19:18 -0500

    "CAMBIOS REALIZADOS 25/11/2022 9:18 p.m JEFF"

---
+++ contrasena/correo.php
@@ -0,0 +1,14 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+?>
